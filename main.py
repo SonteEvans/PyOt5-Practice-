c4b67ce8d118516cba67fccb744e840f9a59700d
Jahsonte <jahsonteevans@gmail.com>
2025-01-26 13:37:29 -0700

    BluePrint

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QToolBar, QStatusBar, QLabel, QComboBox)
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Customer Experience')
+        
+        label = QLabel('Welcome!')
+        label.setAlignment(Qt.AlignCenter)
+        
+        self.setCentralWidget(label)
+        
+        toolbar = QToolBar('Nav bar')
+        
+        
+        self.addToolBar(toolbar)
+    
+    def onNavBarButtonClick(self, s):
+        print("click", s)
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
